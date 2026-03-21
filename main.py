2d6536b2613360935889e9ba7eb14054d48e58a2
mRarogiewicz <amttoo@gmail.com>
2026-03-21 22:33:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import random
 import string
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException, Query
 
 app = FastAPI()
 
+# Načítanie kľúča z prostredia Renderu
+# Ak kľúč v Renderi neexistuje, použije sa "default_tajny_kluc" (len pre testovanie)
+VALID_API_KEY = os.environ.get("API_KEY")
+
 @app.get("/")
-def get_random_string():
-    # Vygeneruje náhodný reťazec o dĺžke 10 znakov
+def get_random_string(key: str = Query(None)):
+    # 1. Kontrola, či používateľ vôbec poslal kľúč
+    if not key:
+        raise HTTPException(status_code=401, detail="Chyba: Chýba API kľúč.")
+
+    # 2. Porovnanie zaslaného kľúča s tým v Renderi
+    if key != VALID_API_KEY:
+        raise HTTPException(status_code=403, detail="Chyba: Nesprávny API kľúč.")
+
+    # 3. Ak je kľúč správny, vygeneruje sa string
     letters = string.ascii_letters
     result_str = ''.join(random.choice(letters) for i in range(10))
     
